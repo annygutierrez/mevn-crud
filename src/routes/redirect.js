df4158a245fdef3d0b9b60eab052321a12d6bc18
Anny Guttierrez <agutilop@everis.com>
2019-08-12 16:30:36 -0500

    Update redirect
    - Update redirect.

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -7,8 +7,8 @@ var fs = require('fs');
 
 router.get('/:url', (req, res) => {
     // res.json({ url: req.params.url });
-    res.redirect(req.params.url);
-    res.end();
+    let urlParam = encodeURIComponent(req.params.url);
+    res.redirect(urlParam);
 });
 
 // ruta que obtiene los datos almacenados en el esquema Usuario
