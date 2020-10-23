d20a0be01b984e349951fe5962eddf95df046527
annygutierrez16 <agutilop@everis.com>
2020-10-23 18:12:03 -0500

    Change procedures
    - Change procedures model.

--- src/models/Tramites.js
+++ src/models/Tramites.js
@@ -4,26 +4,125 @@ const Schema = mongoose.Schema;
 
 
 
+// {
+//     "idTypeRequest": 2,
+//     "name": "Gastos de Sepelio",
+//     "idCategory": 1,
+//     "nameCategory": "Categoria 1",
+//     "description": "Descripcion de gastos de sepelio",
+//     "descriptionLarge": null,
+//     "informationImportant": "1",
+//     "inBeneficiary": "1",
+//     "inComplexity": "1",
+//     "dateRegister": "2010-10-10",
+//     "requirements": [
+//         {
+//             "idRequirement": 1,
+//             "nameRequirement": "Requirimiento 1"
+//         },
+//         {
+//             "idRequirement": 2,
+//             "nameRequirement": "Requirimiento 2"
+//         }
+//     ],
+//     "stages": [
+//         {
+//             "idStage": 1,
+//             "nameStage": "estapa 1"
+//         },
+//         {
+//             "idStage": 2,
+//             "nameStage": "etapa 2"
+//         }
+//     ],
+//     "documents": [
+//         {
+//             "idGroupDocument": 1,
+//             "nameGroupDocument": "Grupo Documento 1",
+//             "documents": [
+//                 {
+//                     "idDocument": 1,
+//                     "nameDocument": "Documento 1"
+//                 },
+//                 {
+//                     "idDocument": 3,
+//                     "nameDocument": "Documento 3"
+//                 }
+//             ]
+//         },
+//         {
+//             "idGroupDocument": 2,
+//             "nameGroupDocument": "Grupo Documento 2",
+//             "documents": [
+//                 {
+//                     "idDocument": 2,
+//                     "nameDocument": "Documento 2"
+//                 },
+//                 {
+//                     "idDocument": 3,
+//                     "nameDocument": "Documento 3"
+//                 }
+//             ]
+//         }
+//     ],
+//     "status": [
+//         {
+//             "idStatus": 1,
+//             "nameStatus": "ACCEPTED",
+//             "duration": 2
+//         },
+//         {
+//             "idStatus": 2,
+//             "nameStatus": "REGISTERED",
+//             "duration": 5
+//         }
+//     ]
+// }
+
+
 /**
  * Se crea el esquema del modelo Usuario con sus propiedades 
  */
 const Tramites = new Schema({
-    id: { type: Number},
+    idTypeRequest: { type: Number },
     name: { type: String},
-    subtitle: { type: String },
-    description: { type: String},
-    list: [{
-        type: String,
-        required: false
-    }],
-    information: [
+    idCategory: { type: Number },
+    nameCategory: { type: String},
+    description: { type: String },
+    descriptionLarge: { type: String },
+    informationImportant: { type: String },
+    inBeneficiary: { type: String },
+    inComplexity: { type: String },
+    dateRegister: { type: String },
+    requirements: [
         {
-            title: { type: String},
-            description: { type: String },
-            list: [{
-                type: String,
-                required: false
-            }]
+            idRequirement: { type: Number },
+            nameRequirement: { type: String }
+        }
+    ],
+    stages: [
+        {
+            idStage: { type: Number },
+            nameStage: { type: String }
+        }
+    ],
+    documents: [
+        {
+            idGroupDocument: { type: Number },
+            nameGroupDocument: { type: String },
+            documents: [
+                {
+                    idDocument: { type: Number },
+                    nameDocument: { type: String }
+                }
+            ]
+        }
+    ],
+    status: [
+        {
+            idStatus: { type: Number },
+            nameStatus: { type: String },
+            duration: { type: Number }
         }
     ]
 },{
